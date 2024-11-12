26ea01c631bb0854d4a4b3705cf7cefec7c11abf
OmarSandoval <121540954+OmarSandoval@users.noreply.github.com>
2024-11-12 17:02:04 +0000

    cambios 12 de nov

--- dictionary.c
+++ dictionary.c
@@ -8,4 +8,27 @@ int initializeDataDictionary(const char *dictionaryName) {
     FILE *dictionary = fopen(dictionaryName, "w+");
     fwrite(&mainheader, sizeof(mainheader), 1, dictionary);
     return EXIT_SUCCESS;
+
+    void reorderEntities(FILE* dataDictionary, ENTITY newEntity, long newEntityDirection)
+    {
+        long entityDataPointer=0;
+        long entityDirection;
+        ENTITY currentEntity;
+
+        fseek(dataDictionary, entityDataPointer, SEEK_SET);
+
+        fread(&entityDirection, sizeof(long),1, dataDictionary);
+        while(entityDirection !=1)
+        {
+            fseek(dataDictionary, entityDirection, SEEK_SET);
+            fread(&currentEntity.name, DATA_BLOCK_SIZE,1, dataDictionary);
+            fread(currentEntity.dataPointer,sizeof(long),1, dataDictionary);
+            fread(currentEntity.attributesPointer, sizeof(long), 1, dataDictionary);
+            entityDataPointer = ftell(dataDictionary);
+            fread(&currentEntity.nextEntity, sizeof(long), 1, dataDictionary);
+            entityDirection = currentEntity.nextEntity;
+        }
+        fseek(dataDictionary, entityDataPointer, SEEK_SET);
+        fwrite(&newEntityDirection, sizeof(long), 1, dataDictionary)
+    }
 }
--- main.c
+++ main.c
@@ -4,8 +4,17 @@ int main(int argc, char** argv) {
     char name[50];
     printf("Enter a data dictonary name; ");
     fgets(name, sizeof(name), stdin);
+    FILE* dataDictionary = initializeDataDictionary(name);
+    ENTITY newEntity;
+    printf("\nEnter the Entity name: ");
 
-    initializeDataDictionary(name);
+    fgets(newEntity.name, sizeof(newEntity.name), stdin);
+    newEntity.dataPointer =EMPTY_POINTER;
+    newEntity.attributesPointer = EMPTY_POINTER;
+    newEntity.nextEntity = EMPTY_POINTER;
+
+    long entityDirection = appendEntity(dataDictionary, newEntity);
+    reorderEntities(dataDictionary, newEntity, entityDirection);
 
     return 0;
 }
